203f8688ee700276a5957e3c053493cc4cf965de
denis-torre <denis.torre@mssm.edu>
2017-09-26 19:35:33 -0400

    Added differential expression

--- js/cycif-heatmap.js
+++ js/cycif-heatmap.js
@@ -1,5 +1,5 @@
 
-function drawCircos(error, drugs, annotations, cytosolicFC, nuclearFC) {
+function drawCircos(error, drugs, annotations, cytosolicFC, nuclearFC, gene='ERK-1') {
   var width = 800;//document.getElementsByClassName('mdl-card__supporting-text')[0].offsetWidth
   var circosHeatmap = new Circos({
         container: '#heatmapChart',
@@ -12,7 +12,10 @@ function drawCircos(error, drugs, annotations, cytosolicFC, nuclearFC) {
         block_id: d.drug_name,
         start: parseInt(d.start),
         end: parseInt(d.end),
-        value: parseFloat(d.logfc)
+        value: -parseFloat(d.logfc),
+        concentration: parseFloat(d.concentration),
+        timepoint: parseInt(d.timepoint),
+        bonferroni_pvalue: parseFloat(d.bonferroni_pvalue)
       };
     })
     nuclearFC = nuclearFC.map(function(d) {
@@ -20,15 +23,35 @@ function drawCircos(error, drugs, annotations, cytosolicFC, nuclearFC) {
         block_id: d.drug_name,
         start: parseInt(d.start),
         end: parseInt(d.end),
-        value: parseFloat(d.logfc)
+        value: -parseFloat(d.logfc),
+        concentration: parseFloat(d.concentration),
+        timepoint: parseInt(d.timepoint),
+        bonferroni_pvalue: parseFloat(d.bonferroni_pvalue)
+      };
+    })
+    drugConcentration = annotations.map(function(d) {
+      return {
+        block_id: d.drug_name,
+        start: parseInt(d.start),
+        end: parseInt(d.end),
+        value: parseFloat(d.concentration),
+        timepoint: parseInt(d.timepoint),
+      };
+    })
+    timepoint = annotations.map(function(d) {
+      return {
+        block_id: d.drug_name,
+        start: parseInt(d.start),
+        end: parseInt(d.end),
+        value: parseInt(d.timepoint),
       };
     })
     circosHeatmap
       .layout(
         drugs,
         {
-          innerRadius: width / 2 - 180,
-          outerRadius: width / 2 - 150,
+          innerRadius: width / 2 - 200,
+          outerRadius: width / 2 - 170,
           ticks: {display: false},
           labels: {
             position: 'center',
@@ -39,25 +62,53 @@ function drawCircos(error, drugs, annotations, cytosolicFC, nuclearFC) {
           }
         }
       )
-      .heatmap('cytosolicFC', cytosolicFC, {
-        innerRadius: 0.8,
+      .heatmap('nuclearFC', nuclearFC, {
+        innerRadius: 0.75,
         outerRadius: 0.99,
         logScale: false,
-        color: 'RdBu'
+        color: 'RdBu',
+        tooltipContent: function (d) {
+          // return d.block_id+' | '+d.concentration+'µm, '+d.timepoint+'h<br>logFC: '+-d.value.toFixed(2)+'<br>(vs DMSO '+d.timepoint+'h)'//+'<br>Bonferroni P: '+d.bonferroni_pvalue.toExponential()
+          return d.block_id+' | '+d.concentration+'µm, '+d.timepoint+'h<br>logFC: '+-d.value.toFixed(2)+'<br>(vs DMSO '+d.timepoint+'h)<br><img src="/images/differential_expression/'+gene+'-'+d.block_id+'-'+d.concentration+'-nuclear-'+d.timepoint+'h.png" style="margin: 5px 10px;">'//+'<br>Bonferroni P: '+d.bonferroni_pvalue.toExponential()
+        }
+
       })
-      .heatmap('nuclearFC', nuclearFC, {
-        innerRadius: 0.7,
-        outerRadius: 0.79,
+      .heatmap('cytosolicFC', cytosolicFC, {
+        innerRadius: 1.01,
+        outerRadius: 1.20,
         logScale: false,
-        color: 'RdBu'
+        color: 'RdBu',
+        tooltipContent: function (d) {
+          return d.block_id+' | '+d.concentration+'µm, '+d.timepoint+'h<br>logFC: '+-d.value.toFixed(2)+'<br>(vs DMSO '+d.timepoint+'h)<br><img src="/images/differential_expression/'+gene+'-'+d.block_id+'-'+d.concentration+'-cytosolic-'+d.timepoint+'h.png" style="margin: 5px 10px;">'//+'<br>Bonferroni P: '+d.bonferroni_pvalue.toExponential()
+          // return d.block_id+' | '+d.concentration+'µm, '+d.timepoint+'h<br>logFC: '+-d.value.toFixed(2)+'<br>(vs DMSO '+d.timepoint+'h)<br><embed type="application/pdf" src="/images/differential_expression/'+d.block_id+'-'+'ERK-1'+'-'+d.concentration+'-'+'nuclear'+d.timepoint+'h"></img>'//+'<br>Bonferroni P: '+d.bonferroni_pvalue.toExponential()
+        }
+      })
+      .heatmap('drugConcentration', drugConcentration, {
+        innerRadius: 1.21,
+        outerRadius: 1.30,
+        logScale: true,
+        color: 'Blues',
+        tooltipContent: function (d) {
+          return d.block_id+' | '+d.value+'µM'
+        }
+      })
+      .histogram('timepoint', timepoint, {
+        innerRadius: 1.31,
+        outerRadius: 1.40,
+        color: 'OrRd',
+        tooltipContent: function (d) {
+          return d.value+'h timepoint'
+        }
       })
       .render()
 }
 
+gene = 'ERK-1'
+
 d3.queue()
   .defer(d3.json, './data/drugs.json')
-  .defer(d3.csv, './data/differential_expression/Actin-differential_expression_cytosolic.txt')
-  .defer(d3.csv, './data/differential_expression/Actin-differential_expression_cytosolic.txt')
-  .defer(d3.csv, './data/differential_expression/Actin-differential_expression_nuclear.txt')
+  .defer(d3.csv, './data/differential_expression/'+gene+'-differential_expression_cytosolic.txt')
+  .defer(d3.csv, './data/differential_expression/'+gene+'-differential_expression_cytosolic.txt')
+  .defer(d3.csv, './data/differential_expression/'+gene+'-differential_expression_nuclear.txt')
   // .defer(d3.csv, './data/days-off.csv')
   .await(drawCircos)
